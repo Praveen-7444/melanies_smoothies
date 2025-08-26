4a03ecb38b4321731573805c004c04a4fc4e0dd6
Praveen M <praveenm7111@gmail.com>
2025-08-26 23:00:06 +0530

    simple app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,55 @@
-# Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 
-# Write directly to the app
-st.title(f":cup_with_straw: Customize your smoothie :cup_with_straw:")
-st.write(
-  """choose the fruits you want in your smoothie!
-  """
-)
-
-# options = st.selectbox('What is your favorite fruit',('Banana','Strawberries', 'Peaches'))
-
-# st.write('you selected :',options)
-
-# session = get_active_session()
-cnx = st.connection('snowflake')
+# Test Snowflake connection
+cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
 
-name_on_order = st.text_input("Name on Smoothie:")
-st.write('The name on your Smoothie will be:', name_on_order)
+test = session.sql("SELECT CURRENT_USER(), CURRENT_ACCOUNT()").collect()
+
+st.subheader("✅ Snowflake Connection Test")
+st.write(test)
 
 
-ingredient_list = st.multiselect('Choose upto five ingredients', my_dataframe, max_selections = 5)
-if ingredient_list:
+# # Import python packages
+# import streamlit as st
+# from snowflake.snowpark.functions import col
 
-    ingredients_string = ''
+# # Write directly to the app
+# st.title(f":cup_with_straw: Customize your smoothie :cup_with_straw:")
+# st.write(
+#   """choose the fruits you want in your smoothie!
+#   """
+# )
+
+# # options = st.selectbox('What is your favorite fruit',('Banana','Strawberries', 'Peaches'))
+
+# # st.write('you selected :',options)
+
+# # session = get_active_session()
+# cnx = st.connection('snowflake')
+# session = cnx.session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+# name_on_order = st.text_input("Name on Smoothie:")
+# st.write('The name on your Smoothie will be:', name_on_order)
+
+
+# ingredient_list = st.multiselect('Choose upto five ingredients', my_dataframe, max_selections = 5)
+# if ingredient_list:
+
+#     ingredients_string = ''
     
-    for fruit_chosen in ingredient_list:
-        ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
+#     for fruit_chosen in ingredient_list:
+#         ingredients_string += fruit_chosen + ' '
+#     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    time_to_insert = st.button('Submit Order')
+#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+#             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+#     time_to_insert = st.button('Submit Order')
 
-    st.write(my_insert_stmt)
+#     st.write(my_insert_stmt)
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+#     if time_to_insert:
+#         session.sql(my_insert_stmt).collect()
+#         st.success('Your Smoothie is ordered!', icon="✅")
