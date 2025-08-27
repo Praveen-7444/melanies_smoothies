ec4c661dbdc06a67d89c34237eb00ce5a637a934
Praveen M <praveenm7111@gmail.com>
2025-08-27 11:34:51 +0530

    Fix dataframe width parameter case sensitivity

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ st.write(
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
-st.dataframe(data=my_dataframe, width='Stretch')
+st.dataframe(data=my_dataframe, width='stretch')
 st.stop()
 
 name_on_order = st.text_input("Name on Smoothie:")
